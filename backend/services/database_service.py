17179a36195c22184d1b16ae90072264ded66ffe
ekosaraju2008 <101073581+ekosaraju2008@users.noreply.github.com>
2025-04-16 07:39:49 -0400

    Update database_service.py

--- backend/services/database_service.py
+++ backend/services/database_service.py
@@ -8,11 +8,11 @@ resources_collection = db.resources  # Collection name
 
 def get_resources(resource_type=None):
     """
-    Fetch resources from MongoDB based on type.
+    Fetch resources from MongoDB based on service type.
     If no type is provided, fetch all resources.
 
-    :param resource_type: (str) "shelter", "food_bank", etc.
+    :param resource_type: (str) "shelter", "food", etc.
     :return: List of matching resources (as dictionaries)
     """
-    query = {"type": resource_type} if resource_type else {}
-    return list(resources_collection.find(query, {"_id": 0}))  # Exclude MongoDB ObjectID
+    query = {"services": {"$in": [resource_type]}} if resource_type else {}
+    return list(resources_collection.find(query, {"_id": 0}))
